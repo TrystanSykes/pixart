7782ebe1583ce001d29fdd6e0a98567d45bf2b5e
Trystan Sykes <trystansykes@hotmail.com>
2017-10-20 14:48:55 +1100

    part 3 complete

--- pixart.js
+++ pixart.js
@@ -6,6 +6,12 @@ $brush = $('.brush');
 $setColor.on('click', function(event){
   event.preventDefault();
   $brush.css('background',$colorField.val());
-
-
 })
+
+var noOfDivs = 1000;
+
+for (i = 0; i < noOfDivs; i++) {
+  $square = $("<div>", {class: 'square'});
+  $('body').append($square)
+};
+
