aed81de5b54e7767e856e721091121acc9fd3deb
Trystan Sykes <trystansykes@hotmail.com>
2017-10-20 15:48:33 +1100

    finished

--- pixart.js
+++ pixart.js
@@ -1,12 +1,34 @@
-$colorField = $('#color-field');
-$setColor = $('#set-color');
-$brush = $('.brush');
+var $colorField = $('#color-field');
+var $setColor = $('#set-color');
+var $brush = $('.brush');
 
 
 $setColor.on('click', function(event){
   event.preventDefault();
   $brush.css('background',$colorField.val());
 })
+
+var $movieField = $('#movie-field');
+var $setMovie = $('#set-movie');
+var $stamp = $('.stamp');
+
+$setMovie.on('click', function(event){
+  event.preventDefault();
+  var options = {
+      url: 'http://www.omdbapi.com',
+      method: 'get',
+      data: {
+        apikey: '2f6435d9',
+        t: $movieField.val()
+      }
+    }
+
+  $.ajax(options).done(function(resp){
+  $stamp.css('background', 'url(' + resp['Poster'] + ')');
+    
+  })
+})
+
 
 var noOfDivs = 1000;
 
@@ -15,9 +37,25 @@ for (i = 0; i < noOfDivs; i++) {
   $('body').append($square)
 };
 
-$squares = $('.square');
+var $squares = $('.square');
+
+var $paintType = $('#paint-type')
+var $choose = $('.choose')
+
+
+var paintValue = $brush.css('background')
+
+
+$choose.on('click', function(){
+  if ($paintType.val() === 'color'){
+    paintValue = $brush.css('background')
+  } else {
+    paintValue = $stamp.css('background')
+  }
+})
+
 
 $squares.on('mouseover', function(event){
-  $(event.target).css('background', $brush.css('background'));
+  $(event.target).css('background', paintValue);
 })
 
